dbcb2cd180e857ed5ed57af141f34a11d9218770
MarcoBione <marcobione.work@gmail.com>
2023-04-13 17:53:29 +0200

    imported bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,27 @@
 <template>
   <div>
-
+    <!-- header -->
+    <HeaderComponent />
+    <!-- main -->
+    <MainComponent />
+    <!-- Footer -->
+    <FooterComponent />
   </div>
 </template>
 
 <script>
-  export default {
-    
+//components import
+import HeaderComponent from './components/HeaderComponent.vue';
+import MainComponent from './components/MainComponent.vue';
+import FooterComponent from './components/FooterComponent.vue';
+
+export default {
+  components: {
+    HeaderComponent,
+    MainComponent,
+    FooterComponent,
   }
+}
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,15 +1,74 @@
-:<template>
-    <div>
+<template>
+    <header>
 
-    </div>
+        <!-- logo DC -->
+        <div class="logo">
+            <img src="../assets/vue-dc-comics-1/img/favicon.ico" alt="dc_logo">
+        </div>
+        <!-- nav bar commands -->
+        <nav>
+            <ul class="d-flex list-unstyled">
+                <li v-for="(item, index) in navBar" @key='index' class="_myitem">
+                    {{ item.name }}
+                </li>
+            </ul>
+        </nav>
+
+    </header>
 </template>
 
 <script>
-    export default {
-        
+
+export default {
+    name: 'HeaderComponent',
+    data() {
+        return {
+            navBar: [
+                {
+                    name: 'Characters',
+                    link: '#'
+                },
+                {
+                    name: 'Comics',
+                    link: '#'
+                },
+                {
+                    name: 'Movies',
+                    link: '#'
+                },
+                {
+                    name: 'TV',
+                    link: '#'
+                },
+                {
+                    name: 'Games',
+                    link: '#'
+                },
+                {
+                    name: 'Collectibles',
+                    link: '#'
+                },
+                {
+                    name: 'Videos',
+                    link: '#'
+                },
+                {
+                    name: 'Fans',
+                    link: '#'
+                },
+                {
+                    name: 'News',
+                    link: '#'
+                },
+                {
+                    name: 'Shop',
+                    link: '#'
+                },
+            ],
+
+        }
     }
+}
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
