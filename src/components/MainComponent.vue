9356c5cba55bbb5c214edd8bbcee030be072a9e6
MarcoBione <marcobione.work@gmail.com>
2023-04-14 16:07:42 +0200

    End main component about links @ MainComponent

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -12,7 +12,7 @@
             <div class="container">
                 <ul class="d-flex list-unstyled p-3 justify-content-between m-0">
                     <li v-for="(item, index) in linkBar" @key='index'
-                        class="fw-semibold text-uppercase d-flex align-items-center">
+                        class="fw-semibold text-uppercase d-flex align-items-center _mypointer">
                         <img :src="item.link" :alt="item.name" class="_myimg">
                         <p class="m-0 px-2 w-100">{{ item.name }}</p>
                     </li>
@@ -27,6 +27,7 @@
 export default {
     data() {
         return {
+
             linkBar: [
                 {
                     name: 'Digital Comics',
