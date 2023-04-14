b82d5d06598a9694ce8ab6cfa663ee7e3e56c465
MarcoBione <marcobione.work@gmail.com>
2023-04-14 09:45:11 +0200

    Add array of links @ footercomponent

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,15 +1,142 @@
-:<template>
+<template>
     <div>
 
     </div>
 </template>
 
 <script>
-    export default {
-        
+export default {
+    data() {
+        return {
+            linksData: [
+                {
+                    category: 'Dc comics',
+                    links: [
+                        {
+                            name: 'Characters',
+                            link: '#',
+                        },
+                        {
+                            name: 'Comics',
+                            link: '#',
+                        },
+                        {
+                            name: 'Movies',
+                            link: '#',
+                        },
+                        {
+                            name: 'TV',
+                            link: '#',
+                        },
+                        {
+                            name: 'Games',
+                            link: '#',
+                        },
+                        {
+                            name: 'Videos',
+                            link: '#',
+                        },
+                        {
+                            name: 'News',
+                            link: '#',
+                        },
+                    ]
+                },
+                {
+                    category: 'Shop',
+                    links: [
+                        {
+                            name: 'Shop DC',
+                            link: '#',
+                        },
+                        {
+                            name: 'Shop DC Collectibles',
+                            link: '#',
+                        }
+                    ]
+                },
+                {
+                    category: 'DC',
+                    links: [
+                        {
+                            name: 'Therms Of Use',
+                            link: '#',
+                        },
+                        {
+                            name: 'Privacy policy (New)',
+                            link: '#',
+                        },
+                        {
+                            name: 'Ad Choises',
+                            link: '#',
+                        },
+                        {
+                            name: 'Advertising',
+                            link: '#',
+                        },
+                        {
+                            name: 'Jobs',
+                            link: '#',
+                        },
+                        {
+                            name: 'Subscriptions',
+                            link: '#',
+                        },
+                        {
+                            name: 'Talent Workshops',
+                            link: '#',
+                        },
+                        {
+                            name: 'CPSC Certificates',
+                            link: '#',
+                        },
+                        {
+                            name: 'Ratings',
+                            link: '#',
+                        },
+                        {
+                            name: 'Shop Help',
+                            link: '#',
+                        },
+                        {
+                            name: 'Contact US',
+                            link: '#',
+                        },
+                    ]
+                },
+                {
+                    category: 'Sites',
+                    links: [
+                        {
+                            name: 'DC',
+                            link: '#',
+                        },
+                        {
+                            name: 'MAD Magazines',
+                            link: '#',
+                        },
+                        {
+                            name: 'DC Kids',
+                            link: '#',
+                        },
+                        {
+                            name: 'DC universe',
+                            link: '#',
+                        },
+                        {
+                            name: 'DC Power Visa',
+                            link: '#',
+                        },
+                        {
+                            name: 'Subscriptions',
+                            link: '#',
+                        }
+                    ]
+                }
+            ]
+        }
     }
+}
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
