3a4ae0fdf9101e0144c568a8d98bf4856b0fc0fb
MarcoBione <marcobione.work@gmail.com>
2023-04-14 09:26:21 +0200

    Add main first & second sections

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,15 +1,58 @@
-:<template>
-    <div>
+<template>
+    <main>
+        <!-- first part -->
+        <div class="text-bg-dark">
+            <div class="container">
+                <h2 class="m-0">---Content Goes Here---</h2>
+            </div>
+        </div>
 
-    </div>
+        <!-- second part -->
+        <div class="text-bg-primary">
+            <div class="container">
+                <ul class="d-flex list-unstyled p-3 justify-content-between m-0">
+                    <li v-for="(item, index) in linkBar" @key='index'
+                        class="fw-semibold text-uppercase d-flex align-items-center">
+                        <img :src="item.link" :alt="item.name" class="_myimg">
+                        <p class="m-0 px-2 w-100">{{ item.name }}</p>
+                    </li>
+                </ul>
+            </div>
+
+        </div>
+    </main>
 </template>
 
 <script>
-    export default {
-        
+export default {
+    data() {
+        return {
+            linkBar: [
+                {
+                    name: 'Digital Comics',
+                    link: '/buy-comics-digital-comics.png'
+                },
+                {
+                    name: 'DC Merchandise',
+                    link: '/buy-comics-merchandise.png'
+                },
+                {
+                    name: 'subscription',
+                    link: '/buy-comics-subscriptions.png'
+                },
+                {
+                    name: 'comic shop locator',
+                    link: '/buy-comics-shop-locator.png'
+                },
+                {
+                    name: 'Dc power visa',
+                    link: '/buy-dc-power-visa.svg'
+                }
+            ],
+
+        }
     }
+}
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
